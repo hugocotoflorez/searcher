e39d7cddce03b0bab5d30fa155315eff42b23400
hugoocf <hugocoto100305@gmail.com>
2021-11-28 18:17:08 +0100

    aaa

--- searcher_instaler.py
+++ searcher_instaler.py
@@ -1,1 +1,3 @@
-import shutil;import os;from subprocess import Popen;os.chdir("\\".join(os.getcwd().split("\\")[:-1]));shutil.rmtree(os.getcwd()+"\\searcher",ignore_errors=True);Popen(["git","clone","https://github.com/hugoocf/searcher.git"])
+import shutil;
+import os;
+from subprocess import Popen;os.chdir("\\".join(os.getcwd().split("\\")[:-1]));shutil.rmtree(os.getcwd()+"\\searcher",ignore_errors=1);Popen(["git","clone","https://github.com/hugoocf/searcher.git"])
